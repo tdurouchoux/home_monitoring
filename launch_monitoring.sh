2fe8f0fd3fb0b180c325d94a2278dce6ed4db9c6
tdurouchoux <thibault.durouchoux@gmail.com>
2023-03-15 21:26:50 +0100

    Config changed for pi installation

--- launch_monitoring.sh
+++ launch_monitoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HOME_MONITORING_DIR='/home/homemonitor/home_monitoring'
-CONFIG_FILE='config/test_configuration.yaml'
-PYTHON_POETRY='/home/homemonitor/.cache/pypoetry/virtualenvs/home-monitoring-Oq8m6lkM-py3.9/bin/python'
+HOME_MONITORING_DIR='/home/pi/home_monitoring'
+CONFIG_FILE='config/pi_configuration.yaml'
+PYTHON_POETRY='/home/pi/.cache/pypoetry/virtualenvs/home-monitoring-P-zH2Pw7-py3.9/bin/python'
 
 cd $HOME_MONITORING_DIR
 $PYTHON_POETRY $HOME_MONITORING_DIR/home_monitoring/launch_monitoring.py $HOME_MONITORING_DIR/$CONFIG_FILE
