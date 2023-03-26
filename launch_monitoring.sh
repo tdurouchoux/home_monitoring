468cddb25a7db277d94653c4deeb2cecefb200a0
tdurouchoux <thibault.durouchoux@gmail.com>
2023-03-26 15:03:22 +0200

    Fixing launch_monitoring script in repo

--- launch_monitoring.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-HOME_MONITORING_DIR='/home/pi/home_monitoring'
-CONFIG_FILE='config/pi_configuration.yaml'
-PYTHON_POETRY='/home/pi/.cache/pypoetry/virtualenvs/home-monitoring-P-zH2Pw7-py3.9/bin/python'
-
-cd $HOME_MONITORING_DIR
-$PYTHON_POETRY $HOME_MONITORING_DIR/home_monitoring/launch_monitoring.py $HOME_MONITORING_DIR/$CONFIG_FILE
